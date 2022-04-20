de7182d0229c1578e9e1c0d2a77866a73d3d86be
Kien Vu <kvstudio.154@gmail.com>
2022-04-20 17:08:48 +0700

    upload database

---
+++ DAO/UserDAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZealEducationManager.Model;
+
+namespace ZealEducationManager.DAO
+{
+    public class UserDAO
+    {
+        ZealEducationManagerDbContext db = null;
+        public UserDAO()
+        {
+            db = new ZealEducationManagerDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.Id;
+        }
+        public bool Update(User entity)
+        {
+            try
+            {
+                var user = db.Users.Find(entity.Id);
+                user.Name = entity.Name;
+                if (!string.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = entity.ModifiedDate;
+                user.Status = entity.Status;
+                user.Phone = entity.Phone;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(int Id)
+        {
+            try
+            {
+                var user = db.Users.Find(Id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,47 @@
+namespace ZealEducationManager.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        public long Id { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [StringLength(32)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(350)]
+        public string Address { get; set; }
+
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        public DateTime? CreateData { get; set; }
+
+        [StringLength(50)]
+        public string CreateBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        [StringLength(50)]
+        public string ModifiedBy { get; set; }
+
+        public bool Status { get; set; }
+
+        [StringLength(250)]
+        public string ProductList { get; set; }
+    }
+}

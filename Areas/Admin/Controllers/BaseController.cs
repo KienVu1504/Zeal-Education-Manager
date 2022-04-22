d858cff588d294eb90cea705bab8b4af0be5c5b2
Kien Vu <kvstudio.154@gmail.com>
2022-04-22 12:16:29 +0700

    recreate home page with shared layout

---
+++ Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZealEducationManager.Common;
+using System.Web.Routing;
+
+namespace ZealEducationManager.Areas.Admin.Controllers
+{
+    public class BaseController:Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new {controller = "Login", action ="Index", Area="Admin"}));
+            }
+            else
+            {
+                if (session.UserName.ToLower() != "admin")
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Index", Area = "Admin" }));
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";//same as style in css
+            }
+            if (type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";//same as style in css
+            }
+            if (type == "success")
+            {
+                TempData["error"] = "alert-danger";//same as style in css
+            }
+        }
+    }
+}

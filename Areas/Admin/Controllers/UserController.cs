e64626af8f8df5952650f73ae60a0aa994c20eb1
Kien Vu <kvstudio.154@gmail.com>
2022-04-23 12:36:36 +0700

    add user management page

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZealEducationManager.DAO;
+
+namespace ZealEducationManager.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 200)
+        {
+            var dao = new UserDAO();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+    }
+}

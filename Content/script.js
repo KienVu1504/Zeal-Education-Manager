5fc1bdbd169572a163c0a1e05c2fd4b42ae9df52
Kien Vu <kvstudio.154@gmail.com>
2022-05-21 14:43:51 +0700

    add geo detector function

--- Content/script.js
+++ Content/script.js
@@ -21,3 +21,50 @@ $(function () {
         }
     })
 })
+
+var x = document.getElementById("user-location");
+
+$(function () {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+        var lat = crd.latitude.toString();
+        var lng = crd.longitude.toString();
+        var coordinates = [lat, lng];
+        console.log(x);
+        getCity(coordinates);
+        return;
+
+    }
+
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+})
+
+function getCity(coordinates) {
+    var xhr = new XMLHttpRequest();
+    var lat = coordinates[0];
+    var lng = coordinates[1];
+    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.686fc1c555414bc569b4d3a02be52e2b&lat=" + lat + "&lon=" + lng + "&format=json", true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest;
+    xhr.addEventListener("readystatechange", processRequest, false);
+
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            var city = response.address.city;
+            var country = response.address.country;
+            x.innerHTML = city + ", " + country;
+            return;
+        }
+    }
+}
